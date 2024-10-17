66111b54ef5de79ffe9fc5e0d6b0c35df79ba92a
Quill <github@inktveer.nl>
2024-10-17 11:24:07 +0200

    trace is klaar

--- Ass2/Trace.cs
+++ Ass2/Trace.cs
@@ -1,5 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Ass2 {
     public class Trace {
-        
+        private Command[] commands;
+
+        public void add(Command step) {
+            commands.Append(step);
+        }
+
+        public int getLength() {
+            return commands.Length;
+        }
+
+        public int getMaxDepth() {
+            return Depth(commands);
+        }
+
+        private static int Depth(Command[] cs) {
+            int  acc        = 0;
+            
+            if (cs == Array.Empty<Command>()) return 0;
+            foreach (var step in cs) {
+                if (step is Repeat) {
+                    Repeat c         = step as Repeat;
+                    int    d         = Depth(c.children);
+                    if (d > acc) acc = d;
+                } 
+            }
+
+            return acc;
+        }
+
+        public int getRepeats() {
+            int acc = 0;
+            foreach (var step in commandsToList(commands)) {
+                if (step is Repeat) acc++;
+            }
+
+            return acc;
+        }
+
+        private static System.Collections.IEnumerable commandsToList(Command[] cs) {
+            foreach (var step in cs) {
+                yield return step;
+                if (step is Repeat) {
+                    Repeat c = step as Repeat;
+                    foreach (var substep in commandsToList(c.children)) {
+                        yield return substep;
+                    }
+                }
+            }
+        }
     }
 }
