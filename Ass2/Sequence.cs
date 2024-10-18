b893d70573df33857425c199d54023cb4f26dd02
Quill <github@inktveer.nl>
2024-10-18 18:35:59 +0200

    doei lelijke indentation

--- Ass2/Sequence.cs
+++ Ass2/Sequence.cs
@@ -4,8 +4,7 @@ using System.Collections.Immutable;
 namespace Ass2;
 
 public abstract class Sequence(Trace commands) {
-    public          Trace    Commands = commands;
-    //public abstract Sequence CreateSequence(List<Command> list);
+    public Trace Commands = commands;
 }
 
 public class BasicSequence(Trace commands) : Sequence(commands) {
