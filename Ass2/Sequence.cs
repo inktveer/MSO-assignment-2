2450317d5dedb36989ca511c4929ab727f121ba8
Quill <github@inktveer.nl>
2024-10-18 18:36:52 +0200

    Betere factory

--- Ass2/Sequence.cs
+++ Ass2/Sequence.cs
@@ -8,19 +8,19 @@ public abstract class Sequence(Trace commands) {
 }
 
 public class BasicSequence(Trace commands) : Sequence(commands) {
-    public static Sequence CreateSequence(List<Command> list) {
+    public static BasicSequence CreateSequence(List<Command> list) {
         return new BasicSequence(new Trace(list));
     }
 }
 
 public class MediumSequence(Trace commands) : Sequence(commands) {
-    public static Sequence CreateSequence(List<Command> list) {
+    public static MediumSequence CreateSequence(List<Command> list) {
         return new MediumSequence(new Trace(list));
     }
 }
 
 public class AdvancedSequence(Trace commands) : Sequence(commands) {
-    public static Sequence CreateSequence(List<Command> list) {
+    public static AdvancedSequence CreateSequence(List<Command> list) {
         return new AdvancedSequence(new Trace(list));
     }
 }
