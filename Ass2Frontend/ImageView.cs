6778c56b73029ba1c94635e5e7f35ba5ab7ff59a
Quill <github@inktveer.nl>
2024-11-12 16:44:52 +0100

    The making of a GTK.Grid in the frontend

--- Ass2Frontend/ImageView.cs
+++ Ass2Frontend/ImageView.cs
@@ -8,18 +8,26 @@ using Grid = Gtk.Grid;
 namespace Ass2Frontend
 {
     public class ImageView : Grid {
-        private int       x;
-        private int       y;
-        private Image[][] images;
+        private Image[,] images;
+        private int      tilesize = 30;
+        private int      X;
+        private int      Y;
         
-        public ImageView(int xSize, int ySize) {
-            x = xSize;
-            y = ySize;
+        public ImageView(int x, int y) {
+            SetSizeRequest(x * tilesize, y * tilesize);
             
         }
 
-        public void Redraw() {
-            
+        public void Redraw(Trace trace, Backend.Grid grid) {
+            for (int x = 0; x < X; x++) {
+                for (int y = 0; y < Y; y++) {
+                    Attach(getImage(grid[x,y]), x, y, tilesize, tilesize);
+                }
+            }
+        }
+
+        private Image getImage(Backend.Grid.Tile tile) {
+            return new Image();
         }
         
     }
--- Ass2Frontend/TextInterface.cs
+++ Ass2Frontend/TextInterface.cs
@@ -1,9 +1,16 @@
 using Gtk;
+using Pango;
+using WrapMode = Gtk.WrapMode;
 
 namespace Ass2Frontend;
 
 public class Textview : TextView {
     public Textview() {
-        
+        WrapMode      = WrapMode.Word;
+        Justification = Justification.Left;
+        AcceptsTab    = false;
+        ModifyFont(FontDescription.FromString("Roboto Mono 16"));
     }
+    
+    
 }
